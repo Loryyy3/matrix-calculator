b492d1e2697e23522183e2e887c6d0110830c646
Lorenzo <lorenzo.leka06@gmail.com>
2025-11-16 12:31:43 +0100

    add a space before each string format to ensure newlines do not create problems

--- src/utils/obtainDim.c
+++ src/utils/obtainDim.c
@@ -3,11 +3,11 @@
 void obtainDim(int *pm, int *pn, double ***M, int **dimM) {
   int m, n;
   do {
-    get_value("#rows m: ", "%d", &m, M, dimM);
+    get_value("#rows m: ", " %d", &m, M, dimM);
   } while (isNotValidDim(m));
 
   do {
-    get_value("#cols n: ", "%d", &n, M, dimM);
+    get_value("#cols n: ", " %d", &n, M, dimM);
   } while (isNotValidDim(n));
 
   *pm = m;
